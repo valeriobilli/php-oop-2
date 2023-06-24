b470f48c8b7d032c69c81bf0393530d5d2906657
Valerio Billi <valerio.billi18@gmail.com>
2023-06-24 18:50:41 +0200

    import cdn bootstrap, fontawesome

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
     require __DIR__ . "models/category.php";
     require __DIR__ . "models/product.php";
 
-
+    $dogsCategory = new category("dog", "fa-solid fa-dog");
+    $catsCategory = new category("cat", "fa-solid fa-cat");
 ?>
 
 <!DOCTYPE html>
@@ -12,9 +13,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <title>Pet Shop</title>
 </head>
 <body>
-    ciao
+    
 </body>
 </html>
