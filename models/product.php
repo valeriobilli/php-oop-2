fef54d734760a4b0a7c31adf0ec28de0e9fd7332
Valerio Billi <valerio.billi18@gmail.com>
2023-06-24 18:43:54 +0200

    construct class product

--- models/product.php
+++ models/product.php
@@ -7,6 +7,16 @@ class Product {
     public $image;
     public $avalaible;
     public $quantity;
+
+    function __construct($name, $price, $category, $image, $avalaible, $quantity);
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->category = $category;
+        $this->image = $image;
+        $this->avalaible = $avalaible;
+        $this->quantity = $quantity;
+    }
 }
 
 ?>
